1ea13755329b93c6444d51b94a1509f29f12faec
Mister1SI <wazzup2690@gmail.com>
2024-01-31 09:39:20 -0600

    Create basic main.c structure

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 gcc -o main main.c src/*.c 
-./main
+./main $1 $2 $3
 
--- main.c
+++ main.c
@@ -1,7 +1,30 @@
+#include <stdio.h>
 
+void print_help();
 
-int main() {
-	
+int main(int argc, char** argv) {
+	if(argc == 1) {
+		print_help();
+		return 0;
+	}
+	if(argc == 2) {
+		if(argv[1] == "-h") {
+			print_help();
+			return 0;
+		} else {
+			puts("Invalid number of arguments. Run with -h for help.");
+			return 1;
+		}
+	}
+	if(argc > 3) {
+		puts("More arguments than expected. Ignoring all past 2.");
+	}
 	return 0;
+}
+
+void print_help() {
+	char* help_text = 
+		"usage: id3edit <param> <file> [options]";
+	puts(help_text);
 }
 
